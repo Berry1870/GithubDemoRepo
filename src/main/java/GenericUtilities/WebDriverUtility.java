c7e6b8dd94f4d1d3b423608086cb1d888c62d92e
Admin <Admin@Admin-PC>
2023-04-13 18:15:02 +0530

    small changes done in WebDriverUtility class

--- src/main/java/GenericUtilities/WebDriverUtility.java
+++ src/main/java/GenericUtilities/WebDriverUtility.java
@@ -5,6 +5,7 @@ public class WebDriverUtility {
 	public static void main(String[] args) {
 		System.out.println("Hi there, this message from Demo WebDriver Utility class");
 
+		System.out.println("Hi there, this is 2nd message");
 	}
 
 }
