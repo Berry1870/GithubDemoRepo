6e8b615a51605b74a32242580c71980ff437ea34
Admin <Admin@Admin-PC>
2023-04-13 18:59:23 +0530

    small changes done by vikrant

--- src/test/java/sample/Demo.java
+++ src/test/java/sample/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hi there, this message from Demo Project for Github practice");
-
+		System.out.println("Hi there, its 6:57 pm");
 	}
 
 }
