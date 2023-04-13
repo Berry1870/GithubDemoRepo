1fc64560222fd5af560799afa53f7941c6841ee9
Nagaveni <Nagaveni@DESKTOP-RSIE0H4>
2023-04-13 19:04:57 +0530

    Comitted by Nagmani

--- src/main/java/GenericUtilities/WebDriverUtility.java
+++ src/main/java/GenericUtilities/WebDriverUtility.java
@@ -6,6 +6,11 @@ public class WebDriverUtility {
 		System.out.println("Hi there, this message from Demo WebDriver Utility class");
 
 		System.out.println("Hi there, this is 2nd message");
+		System.out.println("Hi there, this is 3nd message");
+		System.out.println("Hi there, this is 4nd message");
+
+
+		
 	}
 
 }
--- src/test/java/sample/Demo.java
+++ src/test/java/sample/Demo.java
@@ -6,6 +6,9 @@ public class Demo {
 		
 		System.out.println("Hi there, this message from Demo Project for Github practice");
 		System.out.println("Hi there, its 6:57 pm");
+		System.out.println("Hi there, its 7:05 pm");
+
+		
 	}
 
 }
