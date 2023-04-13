76f2dda1b74de3644dd1c6a29946d36c91dd12a7
Admin <Admin@Admin-PC>
2023-04-13 18:11:24 +0530

    this is first commit on this project..

---
+++ src/main/java/GenericUtilities/WebDriverUtility.java
@@ -0,0 +1,10 @@
+package GenericUtilities;
+
+public class WebDriverUtility {
+
+	public static void main(String[] args) {
+		System.out.println("Hi there, this message from Demo WebDriver Utility class");
+
+	}
+
+}
---
+++ src/test/java/sample/Demo.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi there, this message from Demo Project for Github practice");
+
+	}
+
+}
